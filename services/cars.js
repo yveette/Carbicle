ce0226a9a2aa20e39c5f99fc4895fc82e806a86f
Ivet Dimitrova <dimitrova.v.ivet@gmail.com>
2022-01-24 21:45:16 +0200

    Added create functionality.

--- services/cars.js
+++ services/cars.js
@@ -14,7 +14,7 @@ async function read() {
 
 async function write(data) {
     try {
-        await fs.writeFile(filePath, JSON.stringify(data));
+        await fs.writeFile(filePath, JSON.stringify(data, null, 2));
     } catch {
         console.error('Database write error');
         console.error(err);
@@ -41,11 +41,27 @@ async function getById(id) {
     }
 }
 
+async function createCar(car) {
+    const cars = await read();
+    let id;
+    do {
+        id = nextId();
+    } while (cars.hasOwnProperty(id));
+
+    cars[id] = car;
+    await write(cars);
+}
+
+function nextId() {
+    return 'xxxxxxxx-xxxx'.replace(/x/g, () => (Math.random() * 16 | 0).toString(16));
+}
+
 
 module.exports = () => (req, res, next) => {
     req.storage = {
         getAll,
-        getById
+        getById,
+        createCar
     };
     next();
 }
